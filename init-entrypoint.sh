1b3c425f88786d16de2a7434a096b33d88feffae
Deepak Singhvi <dsinghvi@salesforce.com>
2025-02-23 14:15:07 +0530

    Updated for dockerfile and entrypoing sh
    Updated for dockerfile and entrypoing sh

---
+++ init-entrypoint.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+echo "Running as $(whoami)"
+addgroup -g 7447 abcd
+adduser -u 7447 -G abcd -s /bin/sh -D abcd
+addgroup --system --gid 7447 abcd && adduser -rms /bin/bash --uid 7447 --gid 7447 abcd --create-home
+ls -l /
+ls -l /home
+echo "Creating directory and copying files..."
+mkdir -pv /home/abcd/myfolder/mytestfolder
+echo "Sample text file" > /home/abcd/myfolder/mytestfolder/file1.txt
+echo "Another text file" > /home/abcd/myfolder/mytestfolder/file2.txt
+echo "file created /home/abcd/myfolder/mytestfolder/file1.txt"
+echo "file created /home/abcd/myfolder/mytestfolder/file2.txt"
+chown -R abcd:abcd /home/abcd
+echo "listing /home/abcd"
+ls -l /home/abcd
+chown -R abcd:abcd /home/abcd/myfolder
+echo "listing /home/abcd/myfolder"
+ls -l /home/abcd/myfolder/
+chown -R abcd:abcd /home/abcd/myfolder
+echo "listing /home/abcd/myfolder/mytestfolder"
+ls -l /home/abcd/myfolder/mytestfolder
---
+++ main-entrypoint.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+echo "Running as $(whoami)"
+addgroup -g 7447 abcd
+adduser -u 7447 -G abcd -s /bin/sh -D abcd
+addgroup --system --gid 7447 abcd && adduser -rms /bin/bash --uid 7447 --gid 7447 abcd --create-home
+echo "Listing contents in /home/abcd/myfolder"
+ls -l /home/abcd/myfolder/
+echo "listing /home/abcd/myfolder/mytestfolder"
+ls -l /home/abcd/myfolder/mytestfolder
+echo "main container file" > //home/abcd/myfolder/maincontainerfile1.txt
+chown -R abcd:abcd /home/abcd/myfolder
+echo "file created /home/abcd/myfolder/maincontainerfile1.txt"
+echo "Listing contents in /home/abcd/myfolder"
+ls -l /home/abcd/myfolder/
+echo "listing /home/abcd/myfolder/mytestfolder"
+ls -l /home/abcd/myfolder/mytestfolder
+sleep 3600
