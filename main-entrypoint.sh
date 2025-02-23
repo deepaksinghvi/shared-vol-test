92e32feb36e5a09ede380967ac833ba3a71fae12
Deepak Singhvi <dsinghvi@salesforce.com>
2025-02-23 18:00:22 +0530

    Update main-entrypoint.sh

--- main-entrypoint.sh
+++ main-entrypoint.sh
@@ -8,7 +8,7 @@ ls -l /home/abcd/myfolder/
 echo "listing /home/abcd/myfolder/mytestfolder"
 ls -l /home/abcd/myfolder/mytestfolder
 echo "main container file" > //home/abcd/myfolder/maincontainerfile1.txt
-chown -R abcd:abcd /home/abcd/myfolder
+#chown -R abcd:abcd /home/abcd/myfolder
 echo "file created /home/abcd/myfolder/maincontainerfile1.txt"
 echo "Listing contents in /home/abcd/myfolder"
 ls -l /home/abcd/myfolder/
