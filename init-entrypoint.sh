75b62b1e2703e29c48f4b997979d93b53fe1f92a
Deepak Singhvi <dsinghvi@salesforce.com>
2025-02-24 11:48:20 +0530

    Update init-entrypoint.sh

--- init-entrypoint.sh
+++ init-entrypoint.sh
@@ -3,7 +3,10 @@ echo "Running as $(whoami)"
 addgroup -g 7447 abcd
 adduser -u 7447 -G abcd -s /bin/sh -D abcd
 addgroup --system --gid 7447 abcd && adduser -rms /bin/bash --uid 7447 --gid 7447 abcd --create-home
+echo "ls -l /home/abcd..."
 ls -l /home/abcd
+echo "ls -l /home/abcd/myfolder..."
+ls -l /home/abcd/myfolder
 echo "Creating directory and copying files..."
 mkdir -pv /home/abcd/myfolder/mytestfolder
 echo "Sample text file" > /home/abcd/myfolder/mytestfolder/file1.txt
